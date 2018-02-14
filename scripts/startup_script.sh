b00508d4f7edefdc3287f1e73a5c01b8cd989311
Colby Gallup <letgomylegomon100@gmail.com>
2018-02-13 21:32:22 -0600

    Made script auto-start actually work

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-source /home/nvidia/.profile
-workon 2018
-cd /home/nvidia/1777-2018Vision
+exec > /tmp/startup_script_log
+exec 2>&1
+
+cd /home/nvidia/
+source .profile
+source .virtualenvs/2018/bin/activate
+cd 1777-2018Vision
 
 echo "waiting for camera..."
 while [ "$( ls /dev/video* 2> /dev/null )" = "" ]
@@ -12,4 +16,14 @@ do
 done
 echo "camera found"
 
-python main.py
+echo "waiting for SD card..."
+while [ ! -d "/media/nvidia/My Files" ]
+do
+	echo "..."
+	sleep 1
+done
+echo "SD card found"
+
+nice -n -10 python main.py &
+
+exit 0
